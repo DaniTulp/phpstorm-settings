3619c218e94655c53dbf41e51b344c4db7b3f1b4
Dani Tulp <dani@deboprojects.nl>
2017-11-06 16:36:30 +0100

    PS-173.3531.17 <dani@Danis-MacBook-Pro.local Create ide.general.xml, project.default.xml, diff.xml, git.xml, other.xml, terminal.xml, vagrant.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, colors.scheme.xml, _@user_WarmNeon.icls, Vue File.vue, databaseDrivers.xml, laf.xml, Vuejs.xml, editor.codeinsight.xml, databaseSettings.xml, Blade File༏ext༏.blade.php, editor.xml, applicationLibraries.xml, webServers.xml, debugger.xml, _@user_Solarized Dark.icls

---
+++ fileTemplates/Vue File.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div>$END$</div>
+</template>
+
+<script>
+    //import { mapState, mapActions, mapMutations } from 'vuex';
+    
+	export default {
+		mounted() {
+		},
+		data() {
+			return {
+                
+			};
+		},
+		methods: {
+		}
+	}
+</script>
